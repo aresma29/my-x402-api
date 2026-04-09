9bf9a1398f09fe3818053caed6301af61f72aea9
aresma29 <putraaresma@gmail.com>
2026-04-09 12:09:51 +0700

    fix final railway

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server jalan di:", PORT);
+});
