ec70bae78730bfc3664dcf549987cb4451f2c17e
aresma29 <putraaresma@gmail.com>
2026-04-09 11:04:31 +0700

    reset fix 502

--- service.js
+++ service.js
@@ -1,35 +1,15 @@
 const express = require("express");
-
 const app = express();
 
-// API KEY
-const API_KEY = "my-x402-api-production.up.railway.app";
-
-app.use((req, res, next) => {
-  const key = req.headers["x-api-key"];
-  if (key !== API_KEY) {
-    return res.status(403).json({ error: "Unauthorized" });
-  }
-  next();
-});
-
-// ROUTES
 app.get("/", (req, res) => {
-  res.json({
-    status: "API aktif 🚀",
-    message: "Gunakan /alpha"
-  });
+  res.send("API hidup 🚀");
 });
 
 app.get("/alpha", (req, res) => {
-  res.json({
-    alpha: "Airdrop potensial 🔥"
-  });
+  res.json({ alpha: "OK 🔥" });
 });
 
-// PORT
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running");
 });
