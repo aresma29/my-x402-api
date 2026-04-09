de08b0e99839a3305f27432524bb683dd403d4e6
aresma29 <putraaresma@gmail.com>
2026-04-09 12:21:14 +0700

    debug port

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-// fallback biar gak crash
 const PORT = process.env.PORT || 3000;
+
+console.log("PORT dari Railway:", process.env.PORT);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server jalan di:", PORT);
