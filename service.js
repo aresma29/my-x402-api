ed1703a9d51dfa6aaecd2d30209e1e2023831a6e
aresma29 <putraaresma@gmail.com>
2026-04-09 11:06:05 +0700

    fix port railway

--- service.js
+++ service.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+// ROUTE
 app.get("/", (req, res) => {
   res.send("API hidup 🚀");
 });
@@ -9,7 +10,9 @@ app.get("/alpha", (req, res) => {
   res.json({ alpha: "OK 🔥" });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running");
+// 🔥 INI YANG PALING PENTING
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
