a0bf0311fa04901756b900642c114d5a4a41b4a4
aresma29 <putraaresma@gmail.com>
2026-04-09 11:18:25 +0700

    fix port for railway

--- service.js
+++ service.js
@@ -11,8 +11,8 @@ app.get("/alpha", (req, res) => {
 });
 
 // 🔥 INI YANG PALING PENTING
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
